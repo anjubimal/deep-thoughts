e04d7a99f0a7d86d117c76ac0809416f98ed6e08
anjubimal <anjubimal@tamu.edu>
2021-08-10 14:34:50 -0400

    new commit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -11,13 +13,21 @@ const app = express();
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
-
-
 // integrate our Apollo server with the Express application as middleware
-server.applyMiddleware({ app: server });
+async function startup() {
+  await server.start();
+  server.applyMiddleware({ app });
+}
+startup();
+
+// await server.start();
+
+// // integrate our Apollo server with the Express application as middleware
+// server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
